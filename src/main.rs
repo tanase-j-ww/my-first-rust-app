ba92bd212d9463526fae452a9d16240117ee876b
tanase-j-ww <tanase.j@world-wing.com>
2024-06-09 21:54:32 +0900

    :sparkles: enable to input value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut input = String::new();
+
+    io::stdin()
+     .read_line(&mut input)
+     .expect("failed to read input");
+
+    println!("your input is {}", input);
 }
