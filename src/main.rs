ba452d32fd1a38c461343a1fb8babdb7ad52ea4a
tanase-j-ww <tanase.j@world-wing.com>
2024-06-09 22:08:00 +0900

    :sparkles: use rand package

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Guess number");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
     let mut input = String::new();
 
@@ -9,5 +13,15 @@ fn main() {
      .read_line(&mut input)
      .expect("failed to read input");
 
+    let input: u32 = input.trim().parse().expect("failed to parse input");
+
     println!("your input is {}", input);
+    println!("the secret number is {}", secret_number);
+
+    match input.cmp(&secret_number) {
+        Ordering::Less => println!("too small"),
+        Ordering::Greater => println!("too big"),
+        Ordering::Equal => println!("you selected the correct secret number"),
+    }
+
 }
